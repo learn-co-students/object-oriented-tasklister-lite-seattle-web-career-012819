de7b9a86abe58995d3838b5384c7c53a43afa1df
github username <github email address>
2019-03-18 15:14:39 -0700

    doneish?

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const listContainer = document.getElementById("list");
   const renderApp = () => (listContainer.innerHTML = taskList.render());
 
-  // Add Form Behavior Here!
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const newTask = document.getElementById("new-task-description").value;
+    let item = new TaskListItem(newTask);
+    taskList.tasks.push(item);
+    item.render(listContainer)
+  })
+
 
   renderApp()
+  listContainer.textContent = "My TODO:"
 });
--- src/taskList.js
+++ src/taskList.js
@@ -1,3 +1,18 @@
 class TaskList {
-  
+  constructor(){
+    this.tasks = []
+  }
+
+  render() {
+    let taskList = this.tasks;
+    let container = document.getElementById("list");
+    taskList.forEach(task => {
+      task.render(container);
+    })
+  }
+
+  delete(target_task){
+    this.tasks.filter(task => task.description != target_task.description);
+    this.render();
+  }
 }
