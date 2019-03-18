18109214f53dda5214fa5666bcbf781d07d66778
github username <github email address>
2019-03-18 16:42:52 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
+const taskList = new TaskList();
+
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  
+  const addTaskForm = document.querySelector("#create-task-form");
+  addTaskForm.addEventListener("submit", addTaskOnClick);
 
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
-
-  // Add Form Behavior Here!
 
   renderApp()
 });
+
+function addTaskOnClick(event) {
+  event.preventDefault();
+
+  const form = event.srcElement;
+  const descriptionInput = form.querySelector("#new-task-description");
+  const taskDescription = descriptionInput.value;
+  const prioritySelect = form.querySelector("#priority");
+  const priority = prioritySelect.value;
+  console.log(priority);
+
+  console.log(taskDescription);
+  taskList.addTask(taskDescription, priority);
+
+  renderApp();
+}
+
+function renderApp() {
+  removeAllRenderedTasks();
+
+  const listContainer = document.getElementById("list");
+  const renderedTasks = taskList.render();
+  
+  listContainer.appendChild(renderedTasks);
+}
+
+function removeAllRenderedTasks() {
+  const listContainer = document.getElementById("list");
+  [...listContainer.children].forEach(child => child.remove());
+}
--- src/taskList.js
+++ src/taskList.js
@@ -1,3 +1,33 @@
 class TaskList {
-  
+  constructor() {
+    this.tasks = [];
+    this.idCounter = 0;
+    this.removeTaskById = this.removeTaskById.bind(this);
+  }
+
+  render() {
+    const docFragment = new DocumentFragment;
+    
+    this.tasks
+      .sort((task1, task2)=> task2.priority - task1.priority)
+      .map(taskListItem => taskListItem.generateTaskContainer())
+      .forEach(taskListItem => docFragment.appendChild(taskListItem));
+
+    return docFragment;
+  }
+
+  addTask(taskText, priority) {
+    let id = this.idCounter++;
+    let newTask = new TaskListItem(id, taskText, priority, this);
+    this.tasks.push(newTask)
+  }
+
+  removeTaskById(id) {
+    console.log(id);
+    console.log(this.tasks.find(task => task.id = id));
+    const taskIndex = this.tasks.find(task => task.id === id);
+    this.tasks.splice(taskIndex, 1);
+    console.log(this.tasks)
+  }
+
 }
