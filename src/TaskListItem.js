d5df841c48e8e10a0b860904ef0f4b4445d44be8
ScottDenton <scottyd_14@hotmail.com>
2019-03-19 16:31:36 -0700

    All base goals and first two stretch goals

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,31 @@
 class TaskListItem {
-  
+  constructor(description, priority){
+    this.description = description
+    this.priority = priority
+  }
+
+  deleteItem(e){
+    let li = e.target.parentNode
+    li.parentNode.removeChild(li)
+  }
+
+  render(){
+    let li = document.createElement('li')
+    li.textContent = this.description
+    li.classList.add(this.priority)
+    let button = document.createElement('button')
+    button.textContent = `delete`
+    button.addEventListener('click', this.deleteItem)
+    li.appendChild(button)
+    return li
+
+    // return(
+    // `  <li>
+    //       ${this.description}
+    //       <button data-description="${this.description}">X</button>
+    //     </li>
+    //     `
+    //   )
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
 
   const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const taskList = new TaskList();
+  const renderApp = () => {
+  listContainer.appendChild (taskList.render())
+  }
+
 
   // Add Form Behavior Here!
+  const priority = document.getElementById('priority')
+  const form = document.getElementById('create-task-form')
+  const formDescription = document.getElementById('new-task-description')
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let task = new TaskListItem(formDescription.value, priority.value)
+    taskList.tasks.push(task)
+    listContainer.innerHTML = ''
+    renderApp()
+    form.reset();
+  })
 
-  renderApp()
 });
