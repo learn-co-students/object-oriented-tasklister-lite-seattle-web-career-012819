3c231e958e95b73e2a5e48470b6d420bb29b3894
github username <crishanks12@gmail.com>
2019-03-18 18:18:25 -0700

    Done.

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,23 @@
 class TaskListItem {
+  constructor(text, removeFromTaskListFunction) {
+    this.removeFromTaskListFunction = removeFromTaskListFunction;
+    this.text = text;
+  }
+
+  render() {
+    const li = document.createElement('li');
+    li.textContent = this.text;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
   
+    li.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+      const ul = document.getElementById('task-list');
+      ul.removeChild(li);
+      removeFromTaskListFunction;
+    });
+
+    return li;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 
   const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const renderApp = () => {
+    listContainer.innerHTML = '';
+    listContainer.appendChild(taskList.render());
+  }
 
   // Add Form Behavior Here!
+
+  function newTask() {
+    const taskForm = document.getElementById('create-task-form');
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      
+      // function removeFromList() {
+      //   const index = TaskList.tasks.indexOf(`${input.value}`);
+      //   TaskList.splice(index, 1);
+      // }
+      //pass the above into the taskListItem on like 24.
+      //can move this function's functionality to TaskList and call it add().
+
+      const input = document.getElementById('new-task-description').value;
+      const taskListItem = new TaskListItem(input);
+      taskList.tasks.push(taskListItem);
+      renderApp()
+    });
+  }
+  newTask();
 
   renderApp()
 });
