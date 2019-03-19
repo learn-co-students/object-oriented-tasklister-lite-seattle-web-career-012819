10b1dc02f2dbe81a3032c020d00f9ef7bfe7613a
tiefengeist <sahirnambiar@gmail.com>
2019-03-19 14:00:01 -0700

    Done.

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,26 @@
 class TaskListItem {
-  
+  constructor(content) {
+    this.content = content
+  }
+
+  static newTask() {
+    const description = document.getElementById('new-task-description')
+    return new TaskListItem(description.value)
+  }
+
+  render() {
+    let li = document.createElement('li')
+    let del = document.createElement('button')
+    li.innerHTML = this.content
+    del.addEventListener('click', () => {
+      let index = taskList.tasks.indexOf(li)
+      taskList.tasks.splice(index, 1)
+      li.remove()
+    })
+    del.innerText = "delete"
+    li.appendChild(del)
+
+    return li
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+const taskList = new TaskList();
+
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
 
   const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const renderApp = () => (listContainer.appendChild(taskList.render()));
 
   // Add Form Behavior Here!
+  const submitButton = document.getElementById('submit-button')
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    listContainer.innerText = ''
+    let genTask = TaskListItem.newTask()
+    taskList.addTask(genTask.render())
+    renderApp()
+  })
 
   renderApp()
 });
