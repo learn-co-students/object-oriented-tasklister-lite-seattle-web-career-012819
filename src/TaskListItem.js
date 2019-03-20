cdf02cf81175aa8aec291609a461ed1914b99c70
github username <github email address>
2019-03-19 17:33:31 -0700

    Done.

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,11 @@
 class TaskListItem {
-  
+	constructor(description) {
+		this.description = description;
+	}
+
+	render() {
+		let li = document.createElement('li');
+		li.textContent = this.description;
+		return li;
+	}
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,3 +1,33 @@
 class TaskList {
-  
+	constructor() {
+		this.taskListItemsArr = [];
+	}
+
+	addDatTask(taskListItem) {
+		this.taskListItemsArr.push(taskListItem);
+	}
+
+	// get taskListItemsArr() {
+	// 	return this.taskListItemsArr;
+	// }
+
+	// set taskListItemsArr(Arr) {
+	// 	this.taskListItemsArr = Arr;
+	// }
+
+	render() {
+		let ul = document.createElement('ul');
+		for (const item of this.taskListItemsArr) {
+			ul.appendChild(item.render());
+		}
+		return ul;
+	}
 }
+
+// set diameter(diameter) {
+//    this.radius = diameter / 2
+// }
+
+// get circumference() {
+//    return 2 * Math.PI * this.radius
+// }
